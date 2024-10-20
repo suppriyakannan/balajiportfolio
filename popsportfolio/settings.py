a0575a3c9002c7c1ef3d63d8dba7f651b893292b
suppriyakannan <suppriyakannan@gmail.com>
2024-10-20 12:42:56 +0530

    commit after removing whitenoise for storage

--- popsportfolio/settings.py
+++ popsportfolio/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +126,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # Ensure this is outside your app direct
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # The folder where you keep your app-specific static files
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
